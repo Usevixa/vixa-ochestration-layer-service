29f1b273088808ce0fe7364efe5e1ab0d5ad6969
stacksineden <stacksineden@gmail.com>
2026-04-07 20:28:41 +0100

    updates-on-rsa

--- src/utils/decrypt.js
+++ src/utils/decrypt.js
@@ -5,12 +5,12 @@ import crypto from "crypto";
 // IMPORTANT: Ensure this path or environment variable is correct.
 // If running locally, make sure 'private.pem' is in the root directory.
 
-const rawEnvKey = process.env.PRIVATE_KEY;
-if (!rawEnvKey) {
-  throw new Error("PRIVATE_KEY environment variable is not set.");
-}
+// const rawEnvKey = process.env.PRIVATE_KEY;
+// if (!rawEnvKey) {
+//   throw new Error("PRIVATE_KEY environment variable is not set.");
+// }
 
-const PRIVATE_KEY = Buffer.from(rawEnvKey, 'base64').toString('utf-8');
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 if (!PRIVATE_KEY) {
   throw new Error("PRIVATE_KEY environment variable is not set.");
